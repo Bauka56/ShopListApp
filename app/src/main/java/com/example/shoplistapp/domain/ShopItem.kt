c85fc46c521b17369de7465dacd7a991c0d4428b
Bauka56 <bauyrzanzanysbaj@gmail.com>
2023-04-14 20:03:27 +0600

    change id as undefined to store id in a variable

--- app/src/main/java/com/example/shoplistapp/domain/ShopItem.kt
+++ app/src/main/java/com/example/shoplistapp/domain/ShopItem.kt
@@ -1,9 +1,15 @@
 package com.example.shoplistapp.domain
 
 data class ShopItem(
-    var id: Int,
     var name: String,
     var company: String,
     var count: Int,
-    var enabled: Boolean
-)
+    var enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+){
+
+    companion object{
+
+        const val UNDEFINED_ID = -1
+    }
+}
